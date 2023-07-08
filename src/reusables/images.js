9de6234023c7074a80f80806ccd61136f9067826
harrysink <113678645+harrysink@users.noreply.github.com>
2023-07-08 23:42:46 +0100

    Add files via upload

---
+++ src/reusables/images.js
@@ -0,0 +1,19 @@
+import basket from '../assets/basket.svg';
+import bruchetta from '../assets/bruchetta.svg';
+import creditcard from '../assets/creditcard.svg';
+import DishIcon from '../assets/DishIcon.svg';
+import greeksalad  from '../assets/greeksalad.jpg';
+import homeicon from '../assets/homeicon.svg';
+import lemondessert from '../assets/lemondessert,jpg';
+import Logo from '../assets/Logo.svg';
+import MarioandAdrianA from '../assets/MarioandAdrianA.jpg';
+import MarioandAdrianb from '../assets/MarioandAdrianb.jpg';
+import Recent from '../assets/Recent.svg';
+import restaurantfood from '../assets/restaurantfood.jpg';
+import restaurant from '../assets/restaurant.jpg';
+import restaurantchefB from '../assets/restaurantchefB.jpg';
+import icon_eye_ from '../assets/icon_eye_.svg';
+import icon_hamburger_menu from '../assets/icon_hamburger_menu.svg';
+import bookingConfirmed from '../assets/bookingConfirmed.png'
+
+export default {basket, bruchetta, creditcard, DishIcon, greeksalad, homeicon, lemondessert, Logo, MarioandAdrianA, MarioandAdrianb, Recent, restaurantfood, restaurant, restaurantchefB, icon_eye_, icon_hamburger_menu, bookingConfirmed}
---
+++ src/reusables/reservationInfo.js
@@ -0,0 +1,11 @@
+import MarioandAdrianA from '../assets/MarioandAdrianA.jpg';
+  
+  const reservationInfo = {
+        title: "Reserve",
+        location: "A Table",
+        description: "Make a reservation for you and your guests with ease and comfort",
+        btnname: "Find Table",
+        image: MarioandAdrianA
+    }
+
+export default reservationInfo;
