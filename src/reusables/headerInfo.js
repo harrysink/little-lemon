af930e3b323c6101dc380922669c9a7e957ae8ee
harrysink <113678645+harrysink@users.noreply.github.com>
2023-07-08 23:42:28 +0100

    Create headerInfo.js

---
+++ src/reusables/headerInfo.js
@@ -0,0 +1,11 @@
+import resturantFood from '../assets/restaurantfood.jpg';
+  
+const headerInfo = {
+    title: "Little Lemon",
+    location: "Chicago",
+    description: "We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.",
+    btnname: "Reserve a Table",
+    image: resturantFood
+}
+
+export default headerInfo;
